c740b75b9734fdc6488cfdfb162a72ab72548850
NKT <prem.nikit@gmail.com>
2023-02-23 14:09:17 +0530

    emp_data.py

---
+++ emp_data.py
@@ -0,0 +1,14 @@
+
+
+
+def employee_data() :
+    pass
+
+
+name = input("name : ") #str
+emp_id = int(input("Employee ID: ")) # int
+dept = input("Dept : ") # str
+gender = input("gender : ")# M or F --bool, str
+dob = input("dob : ") #str
+designation = input("designation : ") # str
+place_of_res = input("residance : ")
