26e311978f56f592a81f9666d5188668ad6624a5
NKT <prem.nikit@gmail.com>
2023-02-28 03:57:59 +0530

    infinate_loop.py

---
+++ infinate_loop.py
@@ -0,0 +1,8 @@
+## inf_loop
+
+x = float(input("Enter a positive number :\n"))
+
+while (x < 0):
+    x = float(input("Enter a positive number Please :\n"))
+
+print(f"The positive number you entered is {x}")
